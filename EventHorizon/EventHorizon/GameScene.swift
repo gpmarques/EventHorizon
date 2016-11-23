49bb1348007462e0a4c016bba75d101e0b080e06
gpmarques <guilhermepmarques@globo.com>
2016-11-23 12:10:54 -0200

    Adjustments
    Tap on ship to start
    Drag Objects
    Planet size and strength

--- EventHorizon/EventHorizon/GameScene.swift
+++ EventHorizon/EventHorizon/GameScene.swift
@@ -20,15 +20,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var lastUpdateTime : TimeInterval = 0
     var planetIsClicked = false
     var blackHoleIsClicked = false
-
+    var selectedNode: SKSpriteNode!
+    
     override func sceneDidLoad() {
         self.lastUpdateTime = 0
         print("didLoad")
         entityManager = EntityManager(scene: self)
+        selectedNode = SKSpriteNode()
         self.physicsWorld.contactDelegate = self
     }
     
     override func didMove(to view: SKView) {
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanFrom(recognizer:)))
+        self.view!.addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapShip(recognizer:)))
+        self.view!.addGestureRecognizer(tapGesture)
         
         spaceship = Spaceship(imageNamed: "Spaceship",
                               speed: 100,
@@ -40,37 +48,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVector.zero
     }
     
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if !gameStart {
-            
-            if !planetIsClicked && !blackHoleIsClicked {
-                gameStart = true
-                spaceship.spriteComponent?.physicsBody?.isDynamic = true
-                spaceship.spriteComponent?.physicsBody?.categoryBitMask = CollisionCategory.Collision
-                entityManager.timer.invalidate()
-                entityManager.removeAllCopies()
-                spaceship.component(ofType: TimeComponent.self)?.startTimer()
-            }
-            
+        
             guard let touchedPoint = touches.first?.location(in: self) else {
                 print("Touch location not found")
                 return
             }
             
             if planetIsClicked && !gameStart{
-                print("touchedPoint", touchedPoint)
                 entityManager.spawnPlanet(inThisPoint: touchedPoint)
+                planetIsClicked = false
             }
             
             if blackHoleIsClicked && !gameStart{
-                print("touchedPoint", touchedPoint)
                 entityManager.spawnblackHole(inThisPoint: touchedPoint)
+                blackHoleIsClicked = false
             }
-        }
-        
-        else {
+            
+        } else {
             
             if let orbitComponent = entityManager.find(entityOfType: BlackHole.self)?.component(ofType: OrbitComponent.self) {
                 
@@ -85,9 +82,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
-        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -97,20 +91,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        print(contact.contactPoint)
         if contact.bodyA.node?.name == "Spaceship" &&
-            contact.bodyB.node?.name == "Planet" {
+            (contact.bodyB.node?.name == "Planet" || contact.bodyB.node?.name == "BlackHole") {
             entityManager.remove(spaceship)
         }
+        
         if contact.bodyA.node?.name == "copy" &&
             (contact.bodyB.node?.name == "Planet"
                 || contact.bodyB.node?.name == "BlackHole") {
             contact.bodyA.node?.name = "removeThisCopy"
             entityManager.removeCopy(withThisName: "removeThisCopy")
-        }
-        if contact.bodyA.node?.name == "Spaceship" &&
-            contact.bodyB.node?.name == "BlackHole" {
-            entityManager.remove(spaceship)
         }
         
     }
@@ -143,5 +133,83 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //                blackHole.component(ofType: OrbitComponent.self)?.leaveOrbit()
 //            }
 //        }
+    }
+}
+
+extension GameScene {
+    
+    func panForTranslation(translation: CGPoint) {
+        let position = selectedNode.position
+        guard let name = selectedNode.name else { return }
+        
+        if name == "BlackHole" || name == "Planet" {
+            selectedNode.position = CGPoint(x: position.x + translation.x, y: position.y + translation.y)
+        }
+    }
+    
+    func handlePanFrom(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .began {
+            var touchLocation = recognizer.location(in: recognizer.view)
+            touchLocation = self.convertPoint(fromView: touchLocation)
+            
+            self.selectNodeForTouch(touchLocation: touchLocation)
+        } else if recognizer.state == .changed {
+            var translation = recognizer.translation(in: recognizer.view!)
+            translation = CGPoint(x: translation.x, y: -translation.y)
+            
+            self.panForTranslation(translation: translation)
+            
+            recognizer.setTranslation(CGPoint.zero, in: recognizer.view)
+        }
+    }
+    
+    func degToRad(degree: Double) -> CGFloat {
+        return CGFloat(Double(degree) / 180.0 * M_PI)
+    }
+    
+    func selectNodeForTouch(touchLocation: CGPoint) {
+        // 1
+        let touchedNode = self.atPoint(touchLocation)
+        
+        if touchedNode is SKSpriteNode {
+            // 2
+            if !selectedNode.isEqual(touchedNode) {
+                selectedNode.removeAllActions()
+                selectedNode.run(SKAction.rotate(toAngle: 0.0, duration: 0.1))
+                
+                guard let node = touchedNode as? SKSpriteNode else { return }
+                selectedNode = node
+                // 3
+                if selectedNode.name == "BlackHole" || selectedNode.name == "Planet" {
+                    let sequence = SKAction.sequence([SKAction.rotate(byAngle: degToRad(degree: -4.0), duration: 0.1),
+                                                      SKAction.rotate(byAngle: 0.0, duration: 0.1),
+                                                      SKAction.rotate(byAngle: degToRad(degree: 4.0), duration: 0.1)])
+                    selectedNode.run(SKAction.repeatForever(sequence))
+                }
+            }
+        }
+    }
+    
+}
+
+extension GameScene {
+    func tapShip(recognizer: UITapGestureRecognizer) {
+        var touchLocation = recognizer.location(in: recognizer.view)
+        touchLocation = self.convertPoint(fromView: touchLocation)
+        
+        if recognizer.state == .ended {
+            selectNodeForTouch(touchLocation: touchLocation)
+            if selectedNode.name == "Spaceship" {
+                if !gameStart && !planetIsClicked && !blackHoleIsClicked {
+                    gameStart = true
+                    spaceship.spriteComponent?.physicsBody?.isDynamic = true
+                    spaceship.spriteComponent?.physicsBody?.categoryBitMask = CollisionCategory.Collision
+                    entityManager.timer.invalidate()
+                    entityManager.removeAllCopies()
+                    spaceship.component(ofType: TimeComponent.self)?.startTimer()
+                }
+            }
+        }
+
     }
 }
